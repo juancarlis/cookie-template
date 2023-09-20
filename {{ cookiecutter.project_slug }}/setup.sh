cfc9eba8487fac0ac05a385fbf1ca0098ffd00ad
Juan I. Carlis <juancarlis93@gmail.com>
2023-09-20 19:32:28 -0300

    chore: modify setup.sh
    - add options.
    - incorporate --help.
    - add airflow structure preparation option with docker-compose download.

--- {{ cookiecutter.project_slug }}/setup.sh
+++ {{ cookiecutter.project_slug }}/setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Directorio de logs
+# Log directory
 LOG_DIR="logs/setup"
 mkdir -p $LOG_DIR
 LOG_FILE="$LOG_DIR/setup.log"
 
-# Función para registrar mensajes en el archivo de log y mostrarlos en la consola
+# Function to log messages to the log file and display them on the console
 log() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" | tee -a $LOG_FILE
 }
 
-# Verificar si solo se deben crear los archivos .env
-if [ "$1" == "--env-only" ]; then
-    log "Creando solo archivos .env..."
+create_env_files() {
+    log "Creating .env files..."
     echo 'ENVIRONMENT="DEV"' > .env
     cat <<EOL > .env.dev
 # DEV ENVIRONMENT
@@ -32,41 +31,92 @@ EOL
 # db_name_DATABASE='somedb'
 # db_name_HOST='somehost.com'
 EOL
-    log "Archivos .env creados exitosamente."
-    exit 0
-fi
+    log ".env files created successfully."
+}
 
-# Flujo completo de configuración
-log "Iniciando configuración completa..."
+setup_airflow() {
+    local version="stable"
+    if [[ "$1" == "--version" && -n "$2" ]]; then
+        version="$2"
+    fi
 
-# Crear archivos .env
-log "Creando archivos .env..."
-echo 'ENVIRONMENT="DEV"' > .env
-cat <<EOL > .env.dev
-# DEV ENVIRONMENT
-# DB example structure
-# db_name_TYPE='mysql'
-# db_name_USERNAME='user'
-# db_name_PASSWORD='password'
-# db_name_DATABASE='somedb'
-# db_name_HOST='somehost.com'
-EOL
-cat <<EOL > .env.prd
-# PRD ENVIRONMENT
-# DB example structure
-# db_name_TYPE='mysql'
-# db_name_USERNAME='user'
-# db_name_PASSWORD='password'
-# db_name_DATABASE='somedb'
-# db_name_HOST='somehost.com'
-EOL
-log "Archivos .env creados exitosamente."
+    log "Setting up Airflow..."
+    if [[ ! -d "./airflow" ]]; then
+        mkdir ./airflow
+    fi
 
-# Ejecutar poetry install
-log "Ejecutando poetry install..."
-poetry install 2>&1 | tee -a $LOG_FILE
-log "Poetry install completado."
+    if ! curl -LfO "https://airflow.apache.org/docs/apache-airflow/$version/docker-compose.yaml"; then
+        log "Error: Failed to download Airflow version $version. Please check the available versions at: https://airflow.apache.org/docs/apache-airflow/"
+        exit 1
+    fi
 
-# (Otras acciones que quieras realizar)
+    mv docker-compose.yaml ./airflow/
+    mkdir -p ./airflow/dags ./airflow/logs ./airflow/plugins ./airflow/config
+    echo -e "AIRFLOW_UID=$(id -u)" >> ./airflow/.env
+    log "Airflow setup completed with version $version."
+}
 
-log "Configuración completa finalizada."
+show_help() {
+    case "$1" in
+        airflow)
+            echo "Usage: ./setup.sh airflow [OPTION]"
+            echo "Available options for airflow:"
+            echo "  --help, -h       Display this help message."
+            echo "  --stable         Download the stable version of docker-compose.yaml (default)."
+            echo "  --version v.x.z  Download the specified version of docker-compose.yaml."
+            ;;
+        all)
+            echo "Usage: ./setup.sh all [OPTION]"
+            echo "Available options for all:"
+            echo "  --help, -h       Display this help message."
+            echo "  --no-airflow     Execute the complete setup flow without setting up Airflow."
+            ;;
+        *)
+            echo "Usage: ./setup.sh [OPTION]"
+            echo "Available options:"
+            echo "  --help, -h       Display this help message."
+            echo "  all              Execute the complete setup flow."
+            echo "  env              Only create the .env files."
+            echo "  airflow          Set up Airflow."
+            ;;
+    esac
+}
+
+# Option handling
+case "$1" in
+    --help|-h)
+        show_help
+        ;;
+    all)
+        if [[ "$2" == "--help" || "$2" == "-h" ]]; then
+            show_help all
+            exit 0
+        fi
+        create_env_files
+        if [[ "$2" != "--no-airflow" ]]; then
+            setup_airflow
+        fi
+        log "Executing poetry install..."
+        poetry install 2>&1 | tee -a $LOG_FILE
+        log "Poetry install completed."
+        log "Complete setup finished."
+        ;;
+    env)
+        if [[ "$2" == "--help" || "$2" == "-h" ]]; then
+            show_help env
+            exit 0
+        fi
+        create_env_files
+        ;;
+    airflow)
+        if [[ "$2" == "--help" || "$2" == "-h" ]]; then
+            show_help airflow
+            exit 0
+        fi
+        setup_airflow "$2" "$3"
+        ;;
+    *)
+        echo "Unrecognized or not provided option."
+        show_help
+        ;;
+esac
